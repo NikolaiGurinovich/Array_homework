4da9aee79a26475fcbfa2ca9db81b1840f9a8443
Николай <nikolay.gurinovich@gmail.com>
2023-10-17 13:30:36 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.awt.geom.QuadCurve2D;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double[] array = new double[scanner.nextInt()];
+
+    }
+}
